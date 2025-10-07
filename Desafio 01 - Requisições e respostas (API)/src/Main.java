0f768aacc676a6b7ed625913238b2299d2b48261
Luis Castro <luisr.contato@gmail.com>
2025-10-07 11:56:40 -0300

    Add files via upload

---
+++ Desafio 01 - Requisições e respostas (API)/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um livro para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = ""
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
